4d612f4680bb447b23b939e83e5bd4a921c54ffa
Mahmoud <mahmoud.s.badran@gmail.com>
2020-12-06 03:16:12 +0100

    Add PSR evaluation, Add CosWindow preprocess operation and Edit On Affine

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import cv2
 import os
+from skimage.filters import window
+from skimage.transform import AffineTransform, warp
 
 class MOSSE:
     '''
@@ -27,10 +29,11 @@ class MOSSE:
         -------
         template : cropped Image  
             croped image of the object we want to track ( ground truth ).
-
+            
+        bBox_Pose: croped ROI rectangle coordinates 
         '''
         
-        # selectROI return value : (x , x+x1, y , y+y1) 
+        # croped ROI rectangle coordinates 
         bBox_Pose = cv2.selectROI('test', inputImage, showCrosshair = True)
         
         self.template = inputImage[int(bBox_Pose[1]):int(bBox_Pose[1]+bBox_Pose[3]),
@@ -43,41 +46,57 @@ class MOSSE:
     def getCropedTemplate4Points(self, inputImage): 
         bBox = cv2.selectROI('test', inputImage, showCrosshair = True)
         return bBox
-    
-    # Log transformation 
+       
+    # Log transformation
+    # log function is used to enhance low contrast regions
+    # and provides more edges for the tracker
     def logTransformTemplate(self, template):
         const = 255 / np.log(1 + np.max(template)) 
         logTransformedImage = const * (np.log(template + 1)) 
         #logTransformedImage = np.divide(np.subtract(logTransformedImage,np.mean(logTransformedImage)),np.linalg.norm(logTransformedImage))
-        return np.uint8(logTransformedImage)
+        return logTransformedImage
     
-   
+    
+    def Normalize(self, template):
+        # normalize the template (between 0 and 1) to reduce the lighting 
+        # and solve  any contrast changing issues that could affect the tracking
+        template = template / 255 
+    
+        # centering around the mean
+        template = template - np.mean(template)
+        
+        return template
+        
+    # Apply cosine window to minimize the edge effect in the template 
+    # during tracking 
+    def CosWindow(self, template):
+        cosImage = template * window('hann', template.shape)
+        return cosImage
+    
+        
    # Frequency Domain (DFT)
     def FrequencyDomainTransform(self, template):
-        template = np.abs(np.fft.fft2(template))
-        template = np.fft.fftshift(template)
+        shiftTemplate = np.fft.fftshift(np.fft.fft2(template))
+        template = np.abs(shiftTemplate)
         return template
-    
+       
     # Affine preprocessing 
-    def Affine(self, template):
+    def Affine(self, template): 
         a = -180 / 16
         b = 180 / 16
         r = a + (b - a) * np.random.uniform()
-        rotationMatrix = cv2.getRotationMatrix2D((template.shape[1]/2, template.shape[0]/2), r, 1)
-        rotatedTemplate = cv2.warpAffine(np.uint8(template * 255), rotationMatrix, (template.shape[1], template.shape[0]))
-        rotatedTemplate = rotatedTemplate.astype(np.float32) / 255
-        return rotatedTemplate
+        # rotate the image...
+        matrix_rot = cv2.getRotationMatrix2D((template.shape[1]/2, template.shape[0]/2), r, 1)
+        img_rot = cv2.warpAffine(np.uint8(template * 255), matrix_rot, (template.shape[1], template.shape[0]))
+        img_rot = img_rot.astype(np.float32) / 255
+        return img_rot
     #....................................................................
     
     '''
     # Find A gaussian peak image using the input image and the template where 
     # the target object is centered in the image.
-    Reference github which helped us to implement this function
     '''
     def findSynthaticGaussianPeak(self, inputImage, template, segma = 100):
-        # return a gaussian peak image. using template matching between
-        # the template and the input image by calculating the distance between each pixel
-
         # find center of template (GT) w.r.t the input image
         x_c = template[0] + (template[2] / 2)
         y_c = template[1] + (template[3] / 2)
@@ -91,7 +110,7 @@ class MOSSE:
         array1, array2 = np.meshgrid(width, height) # array1: array of row index values, array2: array of column index values
         
         # Gaussian image
-        gaussianArray = 1 - (np.exp(-((np.square(array1 - x_c) + np.square(array2 - y_c)) / (2 * segma))))
+        gaussianArray = (np.exp(-((np.square(array1 - x_c) + np.square(array2 - y_c)) / (2 * segma))))
         # Gaussian image with gaussian peak centered on the object (g). a ground truth 
         gaussianArray = gaussianArray[int(template[1]):int(template[1]+template[3]),
                               int(template[0]):int(template[0]+template[2])]
@@ -122,12 +141,17 @@ class MOSSE:
         templateFreq = self.FrequencyDomainTransform(template) 
 
         # get template 4 point positions (x, x+x0, y, y+y0)
-        return template,templateFreq, g, G, bBox_Pose
+        return template,templateFreq, g.astype(np.int64), G, bBox_Pose
     
     #.......................................................................
+    
+    def FindPSR(self, peakresponse, maxVal):
+        PSR = (maxVal - np.mean(peakresponse)) / np.std(peakresponse)
+        return PSR
+        
 
-    def InitializeMOSSE(self, template, G, iteration = 128,
-                        learning_Rate= 0.125 , initFlag = 0 ):
+    def InitializeMOSSE(self, template, G, bBox_Pose, iteration = 9,
+                        learning_Rate= 0.125 , initFlag = 0 , A_i=0, B_i=0):
         '''
         Parameters
         ----------
@@ -138,9 +162,12 @@ class MOSSE:
         iteration: number of iterations to train the filter.
         
         learning rate: allows the filter to quickly adapt to appearance
-        changes while still maintaining a robust filter (from the paper)
+        changes while still maintaining a robust filter (from the paper),
+        ranges from [0.01,0.15]
         
         initFlag: a flag value to know if we are in the first frame or not.
+        
+        A_i, B_i : Updated Ai,Bi in each frame
 
         Returns
         -------
@@ -148,30 +175,47 @@ class MOSSE:
 
         '''
         # Resize the G (peak image) to be the same as templateFreq
-        G = cv2.resize(G, dsize=(template.shape[1],template.shape[0]), interpolation=cv2.INTER_CUBIC)
+        G = cv2.resize(G, dsize=(bBox_Pose[2], bBox_Pose[3]), interpolation=cv2.INTER_CUBIC)
         
-        ''' change preprocessing methods here'''
-        #templateFreq = self.FrequencyDomainTransform(random_warp(template))
-        template = self.logTransformTemplate(self.Affine(template))
+        '''preprocessing methods here'''
+        template = cv2.resize(template, (bBox_Pose[2], bBox_Pose[3]))
+        template = self.logTransformTemplate(template)
+        template = self.Normalize(template)
+        template = self.CosWindow(template)
+        
+        # template to Frequency Domain
         templateFreq = self.FrequencyDomainTransform(template)
-        # if we are in the first frame we initilize the MOSSE filter.
-        # Applying the H filter equations presented in the paper
-        Ai = G * np.conjugate(templateFreq)
-        Bi = (templateFreq * np.conjugate(templateFreq))
+        
+ 
         # For the first frame we iterate to initialize the Filter H
         if initFlag == 0:  
+            
+            # if we are in the first frame we initilize the MOSSE filter.
+            # Applying the H filter equations presented in the paper
+            Ai = G * np.conjugate(templateFreq)
+            Bi = (templateFreq * np.conjugate(templateFreq))
+        
             for i in range(iteration):
+                # preprocess again to make data set from the first frame 
+                # with affine affect 
+                templateAffine= self.Affine(template)
+                template = self.logTransformTemplate(templateAffine)
+                template = self.Normalize(template)
+                template = self.CosWindow(template) 
+                templateFreq = self.FrequencyDomainTransform(template)
                 Ai = Ai + (G * np.conjugate(templateFreq))
                 Bi = Bi + (templateFreq * np.conjugate(templateFreq))
+                
             # initial values of the Filter parameters     
             Ai = learning_Rate * Ai
             Bi = learning_Rate * Bi
         else: 
             # From the second frame till the end, Apply the parameters equations with the learning rate for each frame
-            Ai = ((1-learning_Rate) * Ai) + (learning_Rate)*(G * np.conjugate(templateFreq))
-            Bi = ((1-learning_Rate) * Bi) + (learning_Rate)*(templateFreq * np.conjugate(templateFreq))
+            Ai = ((1-learning_Rate) * A_i) + (learning_Rate)*(G * np.conjugate(templateFreq))
+            Bi = ((1-learning_Rate) * B_i) + (learning_Rate)*(templateFreq * np.conjugate(templateFreq))
             
         return Ai, Bi 
+    
     # read frames instead of video 
     def readFrames(self,path):    
         images = []
@@ -215,10 +259,10 @@ class MOSSE:
         
                 #initialize the filter(using only the first frame).
                 # Eq5 in the paper.
-                self.Ai,self.Bi = self.InitializeMOSSE(template, G)
+                self.Ai,self.Bi = self.InitializeMOSSE(template, G, bBox_Pose)
                 i = 1
             else: 
-                # Regulizer
+                # Regularizer
                 reg = 0.01
                 # Filter which will be updated in each frame 
                 Hi = self.Ai/(self.Bi + reg)
@@ -228,11 +272,10 @@ class MOSSE:
                 curr_template = curr_frame_gray[self.trackingWindow[1]:self.trackingWindow[3],
                                     self.trackingWindow[0]:self.trackingWindow[2]]
 
-                curr_template = cv2.resize(curr_template, (self.bBox_Pose[2], self.bBox_Pose[3]))
                 
                 ''' change preprocessing methods here'''
-                curr_template = self.logTransformTemplate(curr_template)
-                #curr_template = pre_process(random_warp(curr_template))
+                curr_template = cv2.resize(curr_template, (self.bBox_Pose[2], self.bBox_Pose[3]))
+               
 
                 # Currrent template in Fourier Space
                 curr_templateF = self.FrequencyDomainTransform(curr_template)
@@ -241,6 +284,7 @@ class MOSSE:
                 # results in a peak image (G) which indicates the new location
                 # of the target object.we are going to call it newObjLoction_G
                 newObjLoction_G = np.conjugate(Hi) * curr_templateF
+
                 '''
                 if(i>19):
                     plt.imshow(Hi)
@@ -270,23 +314,25 @@ class MOSSE:
 
                 # get the current template using the updated tracking winow..
                 curr_template = curr_frame_gray[self.trackingWindow[1]:self.trackingWindow[3], self.trackingWindow[0]:self.trackingWindow[2]]
-                curr_template = cv2.resize(curr_template, (self.bBox_Pose[2], self.bBox_Pose[3]))
                
-                ''' change preprocessing methods here'''
+                '''preprocessing methods here'''
+                curr_template = cv2.resize(curr_template, (self.bBox_Pose[2], self.bBox_Pose[3]))
                 curr_template = self.logTransformTemplate(curr_template)
-                #curr_template = pre_process(random_warp(curr_template))
+                curr_template = self.Normalize(curr_template)
+                curr_template = self.CosWindow(curr_template)
 
-                curr_templateF = self.FrequencyDomainTransform(curr_template)
 
                 # Update A and B
-                self.Ai,self.Bi = self.InitializeMOSSE(curr_templateF, G, iteration=0,initFlag=1)
-
+                self.Ai,self.Bi = self.InitializeMOSSE(curr_template, G,bBox_Pose, iteration=0,initFlag=1)
+                
 
             # Visulize the tracking window 
             cv2.rectangle(curr_frame, (self.trackingWindow[0], self.trackingWindow[1]),
                           (self.trackingWindow[2], self.trackingWindow[3]), (255, 0, 0), 2)
             cv2.imshow('test', curr_frame)
-            cv2.waitKey(100)
+            cv2.waitKey(50)
+            plt.imshow(curr_frame)
+            plt.show()
             # counter 
             i = i + 1 
             
@@ -299,10 +345,16 @@ class MOSSE:
        i = 0
        # list of frames 
        listOfFrames = self.readFrames(path)
+       
+       # PSR Evaluation 
+       listFramesPSR = []
+       
        # Iterate over all the frames in the input video 
        for i in range(len(listOfFrames)):
+           
            # read the current frame 
            curr_frame = listOfFrames[i] 
+           
            # convert to gray 
            curr_frame_gray = cv2.cvtColor(curr_frame, cv2.COLOR_BGR2GRAY)
        
@@ -313,6 +365,7 @@ class MOSSE:
                # create the training data. extract template, gaussian peak.
                # In time(template,g) and frequency domain(templateF,G)
                template,templateF,g,G, bBox_Pose = self.ceateDataSet(curr_frame_gray)
+               
                # current frame tracking window coordinates
                self.bBox_Pose = np.array(bBox_Pose).astype(np.int64)
                self.trackingWindow = np.array([bBox_Pose[0],
@@ -322,13 +375,15 @@ class MOSSE:
        
                #initialize the filter(using only the first frame).
                # Eq5 in the paper.
-               self.Ai,self.Bi = self.InitializeMOSSE(template, G)
+               self.Ai,self.Bi = self.InitializeMOSSE(template, G, bBox_Pose)
               
            else: 
                # Regulizer
                reg = 0.01
+               
                # Filter which will be updated in each frame 
                Hi = self.Ai/(self.Bi + reg)
+    
                              
                # Draw a rectangle bounding box in the current frame which 
                # follows the moving target(by updating the tracking window)
@@ -336,22 +391,42 @@ class MOSSE:
                                    self.trackingWindow[0]:self.trackingWindow[2]]
 
 
+               
+               '''preprocessing methods here'''
                curr_template = cv2.resize(curr_template, (self.bBox_Pose[2], self.bBox_Pose[3]))
                curr_template = self.logTransformTemplate(curr_template)
+               curr_template = self.Normalize(curr_template)
+               curr_template = self.CosWindow(curr_template)
+
+               
                # Currrent template in Fourier Space
                curr_templateF = self.FrequencyDomainTransform(curr_template)
+
                
                # The multiplication of the updated Filter H conjugate and curr_templateF
                # results in a peak image (G) which indicates the new location
                # of the target object.we are going to call it newObjLoction_G
                newObjLoction_G = np.conjugate(Hi) * curr_templateF
-               #newObjLoction_G = np.uint8(np.fft.ifft2(newObjLoction_G))
-                       
+
+                      
                # find the position where the peak is, the center(where we will find the target object)
-               #newObjLoction_G = self.FrequencyDomainTransform(newObjLoction_G)
                max_value = np.max(newObjLoction_G)
                max_pos = np.where(newObjLoction_G == max_value)
+               
+               # PSR evaluation 
+               PSR = self.FindPSR(newObjLoction_G, max_value)
+               
+               if i < 50: 
+                   listFramesPSR.append([i, PSR])
+               if i == 50:
+                   listFramesPSR = np.asarray(listFramesPSR)
+                   plt.plot(listFramesPSR[:,0], listFramesPSR[:,1])
+                   plt.title('PSR Evaluation, tested with 0.125 LR and 0.01 regularizer ')
+                   plt.xlabel('Frames')
+                   plt.ylabel('PSR values')
 
+                   plt.show()
+                   
                changeIn_Y = int(np.mean(max_pos[0]) - (newObjLoction_G.shape[0] * 0.5))
                changeIn_X = int(np.mean(max_pos[1]) - (newObjLoction_G.shape[1] * 0.5))
                
@@ -368,21 +443,24 @@ class MOSSE:
 
                # get the current template using the updated tracking winow..
                curr_template = curr_frame_gray[self.trackingWindow[1]:self.trackingWindow[3], self.trackingWindow[0]:self.trackingWindow[2]]
-               curr_template = cv2.resize(curr_template, (self.bBox_Pose[2], self.bBox_Pose[3]))
-               curr_template = self.logTransformTemplate(curr_template)
-               curr_templateF = self.FrequencyDomainTransform(curr_template)
-               # online update...
-               self.Ai,self.Bi = self.InitializeMOSSE(curr_templateF, G, iteration=0,initFlag=1)
+               
+               # Update A, B 
+               self.Ai,self.Bi = self.InitializeMOSSE(curr_template, G, bBox_Pose, 
+                                                      iteration=0,initFlag=1, A_i=self.Ai, B_i= self.Bi)
 
-
+            
            # Visulize the tracking window 
            cv2.rectangle(curr_frame, (self.trackingWindow[0], self.trackingWindow[1]),
                       (self.trackingWindow[2], self.trackingWindow[3]), (255, 0, 0), 2)
            cv2.imshow('test', curr_frame)
-           cv2.waitKey(50)
+           cv2.waitKey(130)
+        
            # counter 
            i = i + 1 
-        
+       
+       # Plot the PSR evaluation for the first 50 frames 
+       print(listFramesPSR)
+       cv2.destroyAllWindows() 
         
             
             
@@ -392,9 +470,10 @@ start = MOSSE()
 # test data 1 with images
 # your data path    
 path = './Data/test5'      
-#start.trackFrames(path)
+start.trackFrames(path)
 
 # test data video 
 # your video path 
-path = './Data/testVideo.avi'
-start.trackVideo(path)
+#path = './Data/testVideo.avi'
+#path = './Data/test3.mp4'
+#start.trackVideo(path)
